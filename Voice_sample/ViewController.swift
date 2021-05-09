c1d905f4848b459cae4f88c3ef189ac29a8422c2
yuta <yuta.nishimatsu@gmail.com>
2021-05-09 11:21:09 +0900

    Initial Commit

---
+++ Voice_sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Voice_sample
+//
+//  Created by 西松優大 on 2021/05/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
